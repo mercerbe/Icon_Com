6f39687fda53d896d6487701e5eed9db5e258fdd
Admin <mercerbe@gmail.com>
2018-10-17 17:03:06 -0400

    todo: get images imported to cards

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,13 +3,7 @@ import React from "react";
 import { Card } from "semantic-ui-react";
 
 const IconCard = props => (
-  <Card
-    className="iconCards"
-    color="teal"
-    image={props.icon}
-    id={props.id}
-    onClick={() => props.mixIcons(props.id)}
-  />
+  <Card className="iconCards" color="teal" image={props.icon} id={props.id} />
 );
 
 export default IconCard;
